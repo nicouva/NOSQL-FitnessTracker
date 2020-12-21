254374390ad922db1c9e5386f2f609f7c3c56b7e
Nicole Uva <nicouva@gmail.com>
2020-12-21 17:40:05 -0500

    DB UPDATE

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect('mongodb://localhost/workout_db', {
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
